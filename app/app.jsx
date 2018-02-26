5264d1afdf5f18535b1ddf0765f76caecf699533
Jason Coburn <jlewiscoburn@gmail.com>
2018-02-26 16:00:02 -0500

    Setup TodoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -2,17 +2,15 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
-// load Foundation
+const TodoApp = require('TodoApp');
 
-
-
-//require('style!css!foundation-sites/dist/css/foundation-float.min.css');
 
 require('style!css!sass!applicationStyles');
 
 $(document).foundation();
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <TodoApp />,
+  // <p className="text-center">Boilerplate 3 Project</p>,
   document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,11 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components'
+    ],
     alias: {
-
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
@@ -39,7 +42,7 @@ module.exports = {
   },
   sassLoader: {
     includePaths: [
-      path.resolve(__dirname, './node_modules/foundation-sites'/scss)
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
 };
